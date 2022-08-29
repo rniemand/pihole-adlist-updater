49311bd94b18a525f71cc4f8be6d7efe3a3e3ad2
Richard Niemand <niemand.richard@gmail.com>
2022-08-29 16:17:03 -0600

    Seeding initial

---
+++ src/PiHoleListUpdater/AdListParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PiHoleListUpdater;
+
+internal class AdListParser
+{
+  private static Regex TRIM_LINE_RX = new Regex("((\\d{1,3}\\.){3}\\d{1,}|(\\:[^\\s]+))\\s+",
+    RegexOptions.Compiled | RegexOptions.Singleline);
+
+  public void ParseList(HashSet<string> domains, string rawList)
+  {
+    var addedCount = 0;
+    foreach (var line in rawList.Split("\n"))
+    {
+      if (line.StartsWith("#"))
+        continue;
+
+      if (string.IsNullOrWhiteSpace(line))
+        continue;
+
+      var cleanLine = TRIM_LINE_RX.Replace(line, "").Trim();
+      if (string.IsNullOrWhiteSpace(cleanLine))
+        continue;
+
+      if (domains.Contains(cleanLine))
+        continue;
+
+      domains.Add(cleanLine);
+      addedCount++;
+    }
+
+    if (addedCount > 0)
+      Console.WriteLine($"Added {addedCount} new domains");
+  }
+}
---
+++ src/PiHoleListUpdater/WebService.cs
@@ -0,0 +1,37 @@
+﻿namespace PiHoleListUpdater;
+
+internal class WebService
+{
+  private readonly HttpClient _httpClient = new();
+  private readonly bool _usedDevResponses = false;
+  private readonly string[] _devResponseFiles = {
+    "sample-response-01.txt",
+    "sample-response-02.txt",
+    "sample-response-03.txt"
+  };
+  private int _currentResponseIdx = 0;
+
+  public async Task<string> GetUrContentAsync(string url)
+  {
+    if (_usedDevResponses)
+      return GetNextDevResponse();
+
+    Console.WriteLine($"Fetching URL: {url}");
+    var request = new HttpRequestMessage(HttpMethod.Get, url);
+    var response = await _httpClient.SendAsync(request);
+    response.EnsureSuccessStatusCode();
+    return await response.Content.ReadAsStringAsync();
+  }
+
+  private string GetNextDevResponse()
+  {
+    var responseFile = _devResponseFiles[_currentResponseIdx++];
+
+    var readAllText = File.ReadAllText($"TestData\\{responseFile}");
+
+    if (_currentResponseIdx + 1 > _devResponseFiles.Length)
+      _currentResponseIdx = 0;
+
+    return readAllText;
+  }
+}
