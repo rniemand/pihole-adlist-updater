1c1e15ebe7557551dadb0e399e5cc2b6d2d9f23c
Richard Niemand <niemand.richard@gmail.com>
2022-08-31 16:59:06 -0600

    Moving files

--- src/PiHoleUpdater.Common/Misc/SqlTimeOnlyTypeHandler.cs
+++ src/PiHoleUpdater.Common/Misc/SqlTimeOnlyTypeHandler.cs
@@ -1,7 +1,7 @@
 using System.Data;
 using Dapper;
 
-namespace PiHoleUpdater.Common.Repo;
+namespace PiHoleUpdater.Common.Misc;
 
 public class SqlTimeOnlyTypeHandler : SqlMapper.TypeHandler<TimeOnly>
 {
