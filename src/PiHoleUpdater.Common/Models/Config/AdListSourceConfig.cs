c28d5a866dd803b2e5a2e31061428ebff56d61a3
Richard Niemand <niemand.richard@gmail.com>
2022-09-09 20:18:21 -0600

    Updating lists

--- src/PiHoleUpdater.Common/Models/Config/AdListSourceConfig.cs
+++ src/PiHoleUpdater.Common/Models/Config/AdListSourceConfig.cs
@@ -8,6 +8,15 @@ public class AdListSourceConfig
   [YamlMember(Alias = "url")]
   public string Url { get; set; } = string.Empty;
 
+  [YamlMember(Alias = "type")]
+  public string SourceType { get; set; } = string.Empty;
+
+  [YamlMember(Alias = "project_url")]
+  public string ProjectUrl { get; set; } = string.Empty;
+
+  [YamlMember(Alias = "maintainer")]
+  public string Maintainer { get; set; } = string.Empty;
+
   [YamlMember(Alias = "enabled")]
   public bool Enabled { get; set; } = true;
 
